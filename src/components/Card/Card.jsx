a8d2747bcd0dc846631da664f4eb2b35076e3afb
cyfinfaza <42043351+cyfinfaza@users.noreply.github.com>
2021-05-20 16:41:48 -0400

    eqeqeq

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,7 +4,7 @@ import { ReactSVG } from "react-svg";
 
 export default function Card({ type }) {
   const face = type.charAt(1);
-  const colorClass = face == "D" || face == "H" ? "card-red" : "card-black";
+  const colorClass = face === "D" || face === "H" ? "card-red" : "card-black";
   return (
     <div className={componentStyle.cardContainer}>
       <ReactSVG src={"/cards/" + type + ".svg"} className={colorClass} />
